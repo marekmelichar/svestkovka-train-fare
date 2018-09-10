0d9b4956325603c238f0f9e47549544e57f5a346
Marek Melichar <koprivackej@gmail.com>
2018-09-10 18:19:25 +0200

    stash

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const rootRender = document.getElementById('table_of_train_fair')
+const rootRender = document.getElementById('table_of_train_fare')
 
 if (rootRender) {
-  ReactDOM.render(<App />, document.getElementById('table_of_train_fair'));
+  ReactDOM.render(<App />, document.getElementById('table_of_train_fare'));
 }
